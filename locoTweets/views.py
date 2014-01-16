e581809def31cbf48bea42bb225443f13e70cbd4
wkodate <n9lfcth9@gmail.com>
2014-01-17 01:28:45 +0900

    refactor views.py

--- locoTweets/views.py
+++ locoTweets/views.py
@@ -1,17 +1,12 @@
  # -*- coding: UTF-8 -*-
 import twitter
 import secret
-from django.template import Context, loader
 import locoTweets
+from django.template import Context, loader
 from django.http import HttpResponse
  
 def home(req):
-    api = twitter.Api( 
-        consumer_key        = secret.dict['CONSUMER_KEY'],
-        consumer_secret     = secret.dict['CONSUMER_SECRET'],
-        access_token_key    = secret.dict['ACCESS_TOKEN'],
-        access_token_secret = secret.dict['ACCESS_TOKEN_SECRET']
-    )
+    api = getApi()
     terms = secret.terms
 
     tweets = []
@@ -25,31 +20,37 @@ def home(req):
             if (s.coordinates is None):
                 continue
 
-            tweet = {}
-            user_name = s.user.screen_name
-            text = s.text
-            text = text.replace('\n', '')
-            text = text.replace('\r', '')
-            place_name = s.place['full_name']
-            lat = s.coordinates['coordinates'][0]
-            lng = s.coordinates['coordinates'][1]
-
-            tweet['user'] = user_name
-            tweet['texts'] = text
-            tweet['full_names'] = place_name
-            tweet['lat'] = lat
-            tweet['lng'] = lng
+            tweet = extractStatusFields(s)
+            printTweet(tweet) 
             tweets.append(tweet)
-
-            print '[user]'+user_name.encode('utf-8')
-            print '[text]'+text.encode('utf-8')
-            print '[place]'+place_name.encode('utf-8')
-            print "[geo] %f %f" % (lat, lng)
-            print s                
-            print '-----------------------------------'
 
     t = loader.get_template('views.html')
     c = Context({
         'tweets': tweets,
     })
     return HttpResponse(t.render(c))
+
+def getApi():
+    return twitter.Api( 
+        consumer_key        = secret.dict['CONSUMER_KEY'],
+        consumer_secret     = secret.dict['CONSUMER_SECRET'],
+        access_token_key    = secret.dict['ACCESS_TOKEN'],
+        access_token_secret = secret.dict['ACCESS_TOKEN_SECRET']
+    )
+
+def extractStatusFields(status):
+    tweet = {}
+    tweet['user']       = status.user.screen_name
+    tweet['texts']      = status.text.replace('\n', '').replace('\r', '')
+    tweet['full_names'] = status.place['full_name']
+    tweet['lat']        = status.coordinates['coordinates'][0]
+    tweet['lng']        = status.coordinates['coordinates'][1]
+    return tweet
+
+def printTweet(tweet):
+    print '[user]'+tweet['user'].encode('utf-8')
+    print '[text]'+tweet['texts'].encode('utf-8')
+    print '[place]'+tweet['full_names'].encode('utf-8')
+    print "[geo] %f %f" % (tweet['lat'], tweet['lng'])
+    print '-----------------------------------'
+
