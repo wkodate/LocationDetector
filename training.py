de6bd7534b308fd485565339c018ffcf8f4853e9
wkodate <n9lfcth9@gmail.com>
2014-02-01 19:19:49 +0900

    add training script

--- training.py
+++ training.py
@@ -10,23 +10,24 @@ argvs = sys.argv
 argc = len(argvs)
 
 def printDebug():
-    print 'Usage: python %s account' % argvs[0]
+    print 'Usage: python %s account [male|female]' % argvs[0]
     quit()
 
-if argc != 2: 
+if argc != 3 : 
+    printDebug()
+if argvs[2] != 'male' and argvs[2] != 'female':
     printDebug()
 
 account = argvs[1]
+sex = argvs[2]
 count   = 200
 
 t=twitterapi.twitterapi()
-print account+' is ...'
 statuses = t.getUserTimeline(account, count)
 textList=t.getTweets(statuses)
-textString=twitterapi.list2String(textList)
 
 cl=classifier.fisherclassifier(classifier.getwords)
 cl.setdb('test.db')
-print cl.classify(textString)
-
-
+print 'Training ...'
+classifier.sampletrain(cl, textList, sex)
+print 'Success: '+account+' has been trained as '+sex
